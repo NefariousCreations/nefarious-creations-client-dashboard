44f860c4cf18c209901c446fed0b529ac29a4660
Corey Crowden <corey@nefariouscreations.com.au>
2018-03-31 02:53:17 +1100

    Disabled selected default dashboard widgets

--- nefarious-creations-client-dashboard.php
+++ nefarious-creations-client-dashboard.php
@@ -85,3 +85,18 @@ add_action('welcome_panel', function (){ ?>
     <div><!-- .custom-welcome-panel-content -->
 
 <?php });
+
+/**
+ * Remove selected default dashboard widgets
+ */
+add_action( 'admin_init', function () {
+    remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+//    remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+//    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+//    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+});
