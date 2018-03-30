9055f4bdfe3fd10d12cd6aba6f709e2c29024d27
Corey Crowden <corey@nefariouscreations.com.au>
2018-03-31 02:57:30 +1100

    Removed unused message content

--- nefarious-creations-client-dashboard.php
+++ nefarious-creations-client-dashboard.php
@@ -48,38 +48,15 @@ remove_action('welcome_panel', 'wp_welcome_panel' );
 add_action('welcome_panel', function (){ ?>
 
     <div class="custom-welcome-panel-content">
-    <h3><?php _e( 'Welcome back!' ); ?></h3>
-    <p class="about-description"><?php _e( 'This is wear you get things.' ); ?></p>
+    <h1><?php _e( 'Welcome back!' ); ?></h1>
+    <p class="about-description"><?php _e( 'This is where you get things.' ); ?></p>
     <div class="welcome-panel-column-container">
         <div class="welcome-panel-column">
             <h4><?php _e( "Let's Get Started" ); ?></h4>
             <a class="button button-primary button-hero load-customize hide-if-no-customize" href="http://your-website.com"><?php _e( 'Call me maybe !' ); ?></a>
-            <p class="hide-if-no-customize"><?php printf( __( 'or, <a href="%s">edit your site settings</a>' ), admin_url( 'options-general.php' ) ); ?></p>
         </div><!-- .welcome-panel-column -->
         <div class="welcome-panel-column">
-            <h4><?php _e( 'Next Steps' ); ?></h4>
-            <ul>
-                <?php if ( 'page' == get_option( 'show_on_front' ) && ! get_option( 'page_for_posts' ) ) : ?>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-edit-page">' . __( 'Edit your front page' ) . '</a>', get_edit_post_link( get_option( 'page_on_front' ) ) ); ?></li>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-add-page">' . __( 'Add additional pages' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
-                <?php elseif ( 'page' == get_option( 'show_on_front' ) ) : ?>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-edit-page">' . __( 'Edit your front page' ) . '</a>', get_edit_post_link( get_option( 'page_on_front' ) ) ); ?></li>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-add-page">' . __( 'Add additional pages' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-write-blog">' . __( 'Add a blog post' ) . '</a>', admin_url( 'post-new.php' ) ); ?></li>
-                <?php else : ?>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-write-blog">' . __( 'Write your first blog post' ) . '</a>', admin_url( 'post-new.php' ) ); ?></li>
-                    <li><?php printf( '<a href="%s" class="welcome-icon welcome-add-page">' . __( 'Add an About page' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
-                <?php endif; ?>
-                <li><?php printf( '<a href="%s" class="welcome-icon welcome-view-site">' . __( 'View your site' ) . '</a>', home_url( '/' ) ); ?></li>
-            </ul>
-        </div><!-- .welcome-panel-column -->
-        <div class="welcome-panel-column welcome-panel-last">
-            <h4><?php _e( 'More Actions' ); ?></h4>
-            <ul>
-                <li><?php printf( '<div class="welcome-icon welcome-widgets-menus">' . __( 'Manage <a href="%1$s">widgets</a> or <a href="%2$s">menus</a>' ) . '</div>', admin_url( 'widgets.php' ), admin_url( 'nav-menus.php' ) ); ?></li>
-                <li><?php printf( '<a href="%s" class="welcome-icon welcome-comments">' . __( 'Turn comments on or off' ) . '</a>', admin_url( 'options-discussion.php' ) ); ?></li>
-                <li><?php printf( '<a href="%s" class="welcome-icon welcome-learn-more">' . __( 'Learn more about getting started' ) . '</a>', __( 'http://codex.wordpress.org/First_Steps_With_WordPress' ) ); ?></li>
-            </ul>
+            
         </div><!-- .welcome-panel-column welcome-panel-last -->
     </div><!-- .welcome-panel-column-container -->
     <div><!-- .custom-welcome-panel-content -->
